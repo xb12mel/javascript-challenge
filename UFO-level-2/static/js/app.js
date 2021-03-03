d398ab3f20f225cd0fe30bc46560a1162e25e9b2
Melvin Byrd <xb12mel@gmail.com>
2021-03-03 00:21:00 -0500

    adding tonights code and file structure

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -2,8 +2,8 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-var $tbody = d3.select('tbody');
-var button = d3.select('#filter-btn');
+var tbody = d3.select('tbody');
+var filter_button = d3.select('#filter-btn').node().value = "";
 var inputFieldDate = d3.select('#datetime');
 var inputFieldDate = d3.select('#city');
 
@@ -13,28 +13,10 @@ var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes
 // listen for events and search through date/time column to find rows that match user input
 var addInfo = (dataInput) =>{
     dataInput.forEach(ufoSightings =>{
-        var row=$tbody.append("tr");
+        var row=tbody.append("tr");
         columns.forEach(column=> row.append("td").text(ufoSightings[column])
         )
     });
 }
 
 addInfo(tableData);
-
-// listen for events and search through date/time column to find rows that match input
-const newLocal = button.on('click', () => {
-    d3.event.preventDefault();
-    var filterDate = tableData.filter(tableData => tableData.datetime === inputData);
-    $tbody.html("");
-    let response = {
-        filterDate
-    };
-
-    if (response.filterDate.lent !== 0) {
-        addInfo(filterDate);
-    }
-    else {
-        $tbody.append('tr').append('td').text("Results");
-    }
-});
-
